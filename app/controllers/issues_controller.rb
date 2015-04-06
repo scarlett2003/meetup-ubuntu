72ba32ba61d12e4de2db7499808e2c349869ce67
scarlett2003 <jovilee@tpts8.seed.net.tw>
2015-04-06 23:12:32 +0800

    添加網站的耳朵 (create)

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,16 +4,26 @@ class IssuesController < ApplicationController
   	@issue =  Issue.find(params[:id])
   end
 
-  # def destroy
-  # 	issue = Issue.find(params[:id])
-  # 	issue.destroy
-  # 	redirect_to :root
-  # end
+  def new
+    @issue = Issue.new
+  end
+
+  def create
+    Issue.create(issue_params)
+    redirect_to :root
+  end
 
 	def destroy
-	  issue = Issue.find(params[:id])
-	  issue.destroy
+	  i = Issue.find(params[:id])
+	  i.destroy
 	  redirect_to :root
 	end
-  
+
+
+  private
+    def issue_params
+      params.require(:issue).permit(:title, :content)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,12 @@ Rails.application.routes.draw do
   get '/about' => 'page#about'
 
   #issues
+  get '/issues/new' => 'issues#new'
+  get 'issues' => 'issues#index', :as => 'issues'
   get '/issues/:id' => 'issues#show', :as => 'issue'
   delete 'issues/:id' => 'issues#destroy'
+  post '/issues' => 'issues#create'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
